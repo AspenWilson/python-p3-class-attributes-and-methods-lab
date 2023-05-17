0cb6bcff31f9d39e23682ae673177f7c9f9ec338
Aspen Wilson <aspen.schell@gmail.com>
2023-05-17 08:18:53 -0700

    completed lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,49 @@
 class Song:
-    pass
+
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_song_to_count(self)
+        Song.add_to_genres(self)
+        Song.add_to_artists(self)
+        Song.add_to_genre_count()
+        Song.add_to_artist_count()
+
+    @classmethod
+    def add_song_to_count(cls, song):
+        cls.count += 1
+    
+    @classmethod
+    def add_to_genres(cls, song):
+        cls.genres.append(song.genre)
+    
+    @classmethod
+    def add_to_artists(cls, song):
+        cls.artists.append(song.artist)
+    
+    @classmethod
+    def add_to_genre_count(self):
+        Song.genre_count = {}
+        for genre in Song.genres:
+            if genre in Song.genre_count:
+                Song.genre_count[genre] += 1
+            else:
+                Song.genre_count[genre] = 1
+        print (Song.genre_count)
+
+    @classmethod
+    def add_to_artist_count(self):
+        Song.artist_count = {}
+        for artist in Song.artists:
+            if artist in Song.artist_count:
+                Song.artist_count[artist] += 1
+            else:
+                Song.artist_count[artist] = 1
+        print (Song.artist_count)
